8896e390f2bf215d67ca1599cdd71843276a3877
devshah2 <35283231+devshah2@users.noreply.github.com>
2023-04-18 18:08:09 -0500

    augment py start

---
+++ augment.py
@@ -0,0 +1,16 @@
+import cv2
+# given a input low res high res image pair, augment both to generate more training data
+
+def create_dataset(lr, hr):
+    # lr: low resolution image
+    # hr: high resolution image (goal image size not ground truth)
+    # return: a list of augmented low res and high res image pairs
+    rotateCodes=[cv2.ROTATE_180,
+    cv2.ROTATE_90_COUNTERCLOCKWISE,
+    cv2.ROTATE_90_CLOCKWISE]
+    images=[(lr,hr)]
+    for rotate in rotateCodes:
+        lr_rotated = cv2.rotate(lr, rotate)
+        hr_rotated = cv2.rotate(hr, rotate)
+        images.append((lr_rotated, hr_rotated))
+    return images
